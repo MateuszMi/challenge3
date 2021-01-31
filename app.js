93e7590c03e7766d069680d2e18c74ec4f2a52ba
MateuszMi <mati_michna@vp.pl>
2021-01-31 10:02:30 +0100

    teblet__menu

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector("#moblie--menu");
-
-const menuLinks = document.querySelector(".nav__menu")
+const menuLinks = document.querySelector(".nav__menu");
+const item = document.querySelector(".nav__item");
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
